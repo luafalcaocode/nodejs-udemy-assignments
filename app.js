bb7ecca90a50ddea8cda53ae7937864aabc6c966
Luã Falcão <lcodedeveloper@outlook.com>
2020-07-07 16:48:53 -0300

    Assignment 4 - Template Engines

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const path = require('path');
 const mainRouter = require('./routes/index');
 const parser = require('body-parser');
 
-app.use(parser.urlencoded({extended: false}));
+app.use(parser({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(mainRouter);
   
-app.listen(3000); 
+app.listen(3003); 
--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,20 @@ const path = require('path');
 
 
 let username = '';
+let users = [];
 
 router.get('/', (request, response, next) => {
     response.render('home', { pageTitle: 'Home Page'});
 });
 
 router.post('/users', (request, response, next) => {
-    username = request.body;
+    username = request.body.username;
+    users.push({name: username});
     response.redirect('/users');
 });
 
 router.get('/users', (request, response, next) => {
-    response.render('users', { pageTitle: 'Users', user: username });
+    response.render('users', { pageTitle: 'Users', users: users });
 });
 
 
