5b8db2e20d308747589ea05898cd0771290ddba7
Luã Falcão <lcodedeveloper@outlook.com>
2020-07-07 16:49:24 -0300

    Assignment 4 - Template Engines

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const mainRouter = require('./routes/index');
 const parser = require('body-parser');
 
-app.use(parser({extended: false}));
+app.use(parser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
